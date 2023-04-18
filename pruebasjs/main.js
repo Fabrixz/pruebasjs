74449020e2450644958ea65a59316ec2e8124642
Fabrixz <122574416+Fabrixz@users.noreply.github.com>
2023-04-18 01:21:14 -0300

    Add files via upload

---
+++ pruebasjs/main.js
@@ -0,0 +1,13 @@
+let palabra = prompt('ingrese una palabra:')
+alert ('la palabra que ingreso es: '+palabra)
+
+let palabra1 = prompt('ingrese una palabra:')
+alert ('la palabra que ingreso es: '+palabra1)
+
+let palabra2 = prompt('ingrese una palabra:')
+alert ('la palabra que ingreso es: '+palabra2)
+
+function solicitarPalabra () {
+    let palabra2 = prompt('ingrese una palabra:')
+alert ('la palabra que ingreso es: '+palabra2)
+}
